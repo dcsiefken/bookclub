8eb2676c57de52dc2f6660fddba52e4cf0338caa
clay.siefken@nelnet.net <dcsiefken@my365.bellevue.edu>
2024-03-27 20:01:11 -0500

    copy to Module 3

---
+++ Module_3/src/main/java/com/bookclub/BookClubApplication.java
@@ -0,0 +1,17 @@
+/*
+    Clay Siefken (2024) CIS 530 Server-Side Development. Bellevue Univ.
+*/
+
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookClubApplication.class, args);
+	}
+
+}
