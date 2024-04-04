dfb30a7b70206786cea44af78c855432eacce3e9
clay.siefken@nelnet.net <dcsiefken@my365.bellevue.edu>
2024-04-03 19:06:04 -0500

    add 4_1 for discussion testing

---
+++ Module_4_1/src/test/java/com/bookclub/BookClubApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+    Clay Siefken (2024) CIS 530 Server-Side Development. Bellevue Univ.
+*/
+
+package com.bookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
